5c0f601af6dd8f40ec7f03ae9ac26c97d6be0d31
mathemolympiad <kaiserchan1023@gmail.com>
2025-04-05 12:19:32 +0800

    small updates

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 
 app.set('view engine', 'hbs');
 
-// Serve static files from the "public" folder
 app.use(express.static('public'));
 
 app.use(express.json());
@@ -15,10 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     const data = JSON.parse(fs.readFileSync('data/problems.json', 'utf8'));
 
-    // Ensure JSON is safely escaped and stringified
     res.render('index.hbs', {
         problems: JSON.stringify(data, null, 2)
-            .replace(/</g, '\\u003c')  // Prevent HTML injection issues
+            .replace(/</g, '\\u003c')
     });
 });
 
@@ -27,7 +25,6 @@ app.get('/add', (req, res) => {
     res.render('add.hbs', {problems: data.problems});
 })
 
-// handle member registration
 app.post('/add/submit', (req, res) => {
     const { source, timeLimit, problem, answer } = req.body;
     console.log(`Registering ${source}...`);
@@ -38,13 +35,13 @@ app.post('/add/submit', (req, res) => {
     }
 
     try {
-    const problems = JSON.parse(fs.readFileSync('data/problems.json', 'utf8'));
-    problems["problems"].push( { source, timeLimit, problem, answer } );
-    fs.writeFileSync('data/problems.json', JSON.stringify(problems, null, 2));
-    res.redirect('/add');
+      const problems = JSON.parse(fs.readFileSync('data/problems.json', 'utf8'));
+      problems["problems"].push( { source, timeLimit, problem, answer } );
+      fs.writeFileSync('data/problems.json', JSON.stringify(problems, null, 2));
+      res.redirect('/add');
     } catch (error) {
-    console.error(error);
-    res.status(500).send('Error registering the problem!');
+      console.error(error);
+      res.status(500).send('Error registering the problem!');
     }
 });
 
